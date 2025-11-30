bf1c37ada2d2f35c5bd2e6f38877cc544d307580
ayushb980000-spec <ayush.b980000@gmail.com>
2025-11-29 19:31:53 -0800

    Update Ayush.java

--- src/main/java/frc/robot/Ayush.java
+++ src/main/java/frc/robot/Ayush.java
@@ -1,3 +1,10 @@
+package frc;
+
 public class Ayush {
-    
+
+    public static void main(String[] args) {
+        System.out.println("hellooooooo");
+
+        System.out.println("Test?");
+    }
 }
